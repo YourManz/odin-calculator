7bf6b3ebc9e171ba9ddc82b2bcade0e111878c27
Kobe Neilson <kobeneilson@gmail.com>
2023-11-04 19:56:07 -0700

    Will now print out the charecter pressed
    Made it so that the button innerHTML will be console logged if pressed. The opperators are succesfully filtered out.

--- main.js
+++ main.js
@@ -9,11 +9,18 @@ for (let i = 0; i <= 3; i++) {
 
         for (let index=0; index < 4; index++) {
             const buttonType = document.createElement('button');
-
-            console.log(arrayCounter);
-            // buttonType.classList.add(charecterArray[arrayCounter]);
             buttonType.classList.add('regular-button');
             buttonType.innerHTML = `${charecterArray[arrayCounter]}`;
+            if (typeof charecterArray[arrayCounter] === 'string') {
+                buttonType.classList.add(charecterArray[arrayCounter]);
+            }
+            buttonType.addEventListener('click', (event) => {
+                if (event.target.classList.length >= 2) {
+                    pickOperator(event.target.innerHTML)
+                    return;
+                }
+                console.log(event.target.innerHTML)
+            })
 
             rowDiv.appendChild(buttonType);
             arrayCounter++;
